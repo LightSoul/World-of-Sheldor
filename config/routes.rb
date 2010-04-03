c344d5ef4979b3abaae695ae098441c0decf0e45
Robi <robi.tacutu@gmail.com>
2010-04-03 16:35:34 +0300

    User model with passwords, and user show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :recipes
+  map.resources :users
 
   map.contact '/contact', :controller => 'pages', :action => 'contact'
   map.about   '/about',   :controller => 'pages', :action => 'about'
